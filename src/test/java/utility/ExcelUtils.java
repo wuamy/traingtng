92774f029d86c0bead40a74fbcafcf4f65e7491e
amy <wuamyamy@gmail.com>
2015-11-10 09:11:58 -0500

    training  for ng

--- src/test/java/utility/ExcelUtils.java
+++ src/test/java/utility/ExcelUtils.java
@@ -5,7 +5,6 @@ package utility;
  */
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
@@ -31,13 +30,15 @@ public class ExcelUtils {
             int startCol = 1;
             int ci, cj;
             int totalRows = ExcelWSheet.getLastRowNum();
+            //print out the total rows
+            System.out.println(totalRows);
             int totalCols = 2;
             tabArray = new String[totalRows][totalCols];
             ci = 0;
 
             for (int i = startRow; i <= totalRows; i++, ci++) {
                 cj = 0;
-                for (int j = startCol; j <= totalCols; cj++) {
+                for (int j = startCol; j <= totalCols; j++, cj++) {
                     tabArray[ci][cj] = getCellData(i, j);
                     System.out.println(tabArray[ci][cj]);
                 }
@@ -48,7 +49,7 @@ public class ExcelUtils {
         } catch (FileNotFoundException e) {
             System.out.println("Could not read the Excel sheet");
             e.printStackTrace();
-            ;
+
         } catch (IOException e) {
             System.out.println("Could not read the excel sheet");
             e.printStackTrace();
