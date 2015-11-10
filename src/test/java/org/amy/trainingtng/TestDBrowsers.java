ae17c7428553a63729f0783fa504c02c5e319ec2
amy <wuamyamy@gmail.com>
2015-11-10 12:57:35 -0500

    training  for ng

--- src/test/java/org/amy/trainingtng/TestDBrowsers.java
+++ src/test/java/org/amy/trainingtng/TestDBrowsers.java
@@ -5,6 +5,7 @@ package org.amy.trainingtng;
  */
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.BeforeClass;
@@ -27,9 +28,13 @@ public class TestDBrowsers {
         if(browser.equalsIgnoreCase("firefox")){
             driver = new FirefoxDriver();
         }else if(browser.equalsIgnoreCase("ie")){
-            File file = new File("C:/SeleniumPR/IEDriverServer.exe");
-            System.setProperty("webdriver.ie.driver", file.getAbsolutePath());
+            File file1 = new File("C:/SeleniumPR/IEDriverServer.exe");
+            System.setProperty("webdriver.ie.driver", file1.getAbsolutePath());
             driver = new InternetExplorerDriver();
+        }else if(browser.equalsIgnoreCase("chrome")){
+            File file2 = new File("C:/SeleniumPR/chromedriver.exe");
+            System.setProperty("webdriver.chrome.driver", file2.getAbsolutePath());
+            driver = new ChromeDriver();
         }
     }
 }
